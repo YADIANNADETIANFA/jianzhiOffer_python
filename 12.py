4da6aa53e7ef6b2d230898560e1fdd6167309e58
Icarus <871227769@qq.com>
2022-02-15 23:55:02 +0800

    提交

---
+++ 12.py
@@ -0,0 +1,48 @@
+class Solution:
+    def hasPath(self, matrix, word: str) -> bool:
+
+        if matrix is None or str is None:
+            return False
+        if word == '':
+            return True
+
+        row = len(matrix)
+        col = len(matrix[0])
+
+        if row < 1 or col < 1:
+            return False
+
+        for i in range(row):
+            for j in range(col):
+                if self.hasPathCore(matrix, row, col, word, i, j, 0):
+                    return True
+        return False
+
+    def hasPathCore(self, matrix, row, col, word, i, j, index):
+        if i >= row or j >= col or i < 0 or j < 0:
+            return False
+        if matrix[i][j] != word[index]:
+            return False
+        if len(word) - 1 == index:
+            return True
+
+        # 将走过的位置变为不可能出现的字符'0'，False的时候记得换回来
+        temp = matrix[i][j]
+        matrix[i][j] = '0'
+        res = self.hasPathCore(matrix, row, col, word, i - 1, j, index + 1) \
+              or self.hasPathCore(matrix, row, col, word, i + 1, j, index + 1) \
+              or self.hasPathCore(matrix, row, col, word, i, j - 1, index + 1) \
+              or self.hasPathCore(matrix, row, col, word, i, j + 1, index + 1)
+        if res:
+            return True
+        else:
+            matrix[i][j] = temp
+            return False
+
+
+if __name__ == '__main__':
+    soultion = Solution()
+    print(soultion.hasPath([[]], 'abcd'))
+
+# 矩阵中的路径
+# 回溯法
